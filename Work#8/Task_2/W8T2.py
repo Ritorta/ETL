f9f8b84078bb47c7f5a450b1fc9025aff10a1222
Ritorta <Meddesu@yandex.ru>
2024-04-30 20:02:04 +0500

    allmust done

--- Work#8/Task_2/W8T2.py
+++ Work#8/Task_2/W8T2.py
@@ -5,11 +5,12 @@ import pendulum
 from airflow.decorators import dag, task
 from airflow.providers.telegram.operators.telegram import TelegramOperator
 from sqlalchemy import create_engine
+import pandas as pd
 
 os.environ["no_proxy"]="*"
 
 @dag(
-    dag_id="wether-tlegram-sql",
+    dag_id="wether-tlegram-sql-exel",
     schedule="@once",
     start_date=pendulum.datetime(2024, 4, 30, tz="UTC"),
     catchup=False,
@@ -19,13 +20,23 @@ os.environ["no_proxy"]="*"
 
 def WetherETL():
 
-    send_message_telegram_task = TelegramOperator(
+    first_message_telegram = TelegramOperator(
         task_id='send_message_telegram',
         telegram_conn_id='telegram_default',
         token='6875707033:AAG2TaDKrrLUwlUmcX9LIVA1uCm6S43pya0',
         chat_id='547504860',
         text='Wether in Tyumen \nYandex: ' + "{{ ti.xcom_pull(task_ids=['yandex_wether'],key='wether')[0]['temperature']}}" + " degrees at " + "{{ ti.xcom_pull(task_ids=['yandex_wether'],key='wether')[0]['datetime']}}" +
     "\nOpen wether: " + "{{ ti.xcom_pull(task_ids=['open_wether'],key='open_wether')[0]['temperature']}}" + " degrees at " + "{{ ti.xcom_pull(task_ids=['open_wether'],key='open_wether')[0]['datetime']}}",
+    )
+
+
+    second_message_telegram = TelegramOperator(
+        task_id='send_message_telegram_excel',
+        telegram_conn_id='telegram_default',
+        token='6875707033:AAG2TaDKrrLUwlUmcX9LIVA1uCm6S43pya0',
+        chat_id='547504860',
+        text="""<b>Data from excel file:</b> {{ task_instance.xcom_pull(task_ids='data_from_excel') }}""",
+
     )
 
 
@@ -99,11 +110,33 @@ def WetherETL():
         print("Yandex "+str(kwargs['ti'].xcom_pull(task_ids=['yandex_wether'],key='wether')[0])+" Open "+str(kwargs['ti'].xcom_pull(task_ids=['open_wether'],key='open_wether')[0]))
 
 
+    def truncate_message(message, max_length=1000):
+        if len(message) > max_length:
+            return message[:max_length]
+        return message
+    
+
+    @task(task_id='data_from_excel')
+    # def get_data_from_excel():
+    #     excel_file_path = "/home/ritorta/HomeWork/W8/TeL_W8T2.xlsx"
+    #     df = pd.read_excel(excel_file_path)
+    #     table_html = df.to_html(index=False, escape=False)
+    #     truncated_html = truncate_message(table_html)
+    #     return truncated_html
+
+    def get_data_from_excel():
+        excel_file_path = "/home/ritorta/HomeWork/W8/TeL_W8T2.xlsx"
+        df = pd.read_excel(excel_file_path)
+        table_text = df.to_string(index=False)
+        truncated_text = truncate_message(table_text)
+        return truncated_text
+
     yandex_wether = get_yandex_wether()
     open_wether = get_open_wether()
     python_wether = get_wether()
     save_weather = get_save_weather()
+    data_from_excel = get_data_from_excel()
 
-    yandex_wether >> open_wether >> python_wether >> send_message_telegram_task >> save_weather
-
+    yandex_wether >> open_wether >> python_wether >> first_message_telegram >> save_weather >> data_from_excel >> second_message_telegram
+    
 dag = WetherETL()
